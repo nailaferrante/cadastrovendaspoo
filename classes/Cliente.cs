09664658c50e15ea177a6b2420fdc09a353fd9bb
Naila Ferrante <nailaferrante@hotmail.com>
2017-10-27 18:50:12 -0200

    Criação do sumário da Classe Cliente

--- classes/Cliente.cs
+++ classes/Cliente.cs
@@ -1,16 +1,28 @@
 using System;
 
 namespace cadastrovendaspoo.classes{
+    /// <summary>
+    /// A Classe cliente possui informações sobre os dados do cliente.
+    /// </summary>
     public class Cliente{
         private string nome;
         private string email;
         private string cpf;
         private DateTime datacadastro;
-
+        /// <summary>
+        /// Constroi o objeto cliente.
+        /// </summary>
         public Cliente()
         {
             
         }
+        /// <summary>
+        /// Constroi o objeto Cliente e indica os parâmetros necessários.
+        /// </summary>
+        /// <param name="nome">Informe o nome do cliente em formato texto.</param>
+        /// <param name="email">Informe o e-mail do cliente em formato texto.</param>
+        /// <param name="cpf">Informe o CPF do cliente em formato texto.</param>
+        /// <param name="datacadastro">Informe a data do cadastro em formato DateTime.</param>
         public Cliente(string nome, string email, string cpf, DateTime datacadastro)
         {
             this.nome=nome;
